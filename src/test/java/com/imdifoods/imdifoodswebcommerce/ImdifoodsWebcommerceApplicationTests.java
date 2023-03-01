c5c176fb8b04c088fe5083efa31482d36e355eb2
ahmad.naufan <ahmad.naufan@ui.ac.id>
2023-03-01 17:16:46 +0700

    Initialize project

---
+++ src/test/java/com/imdifoods/imdifoodswebcommerce/ImdifoodsWebcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imdifoods.imdifoodswebcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdifoodsWebcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
