63e8b63b17feaad20a1ae77e53b7a972745ed26f
fathanm10 <fathan.muhammad01@ui.ac.id>
2023-03-02 10:27:56 +0700

    [RED] create productTest

--- src/main/java/com/imdifoods/imdifoodswebcommerce/model/Product.java
+++ src/main/java/com/imdifoods/imdifoodswebcommerce/model/Product.java
@@ -1,23 +1,20 @@
 package com.imdifoods.imdifoodswebcommerce.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+@Generated
 @Entity
 @Table(name = "Product")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
-    private String desc;
+    private String description;
     private int stock;
     private int price;
 }
---
+++ src/test/java/com/imdifoods/imdifoodswebcommerce/model/ProductTest.java
@@ -0,0 +1,19 @@
+package com.imdifoods.imdifoodswebcommerce.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class ProductTest {
+
+    @Test
+    void productBuilderTest() {
+        Product product = Product.builder()
+                .name("Ayam")
+                .description("Makanan spesial")
+                .stock(3)
+                .price(10000)
+                .build();
+    }
+}
