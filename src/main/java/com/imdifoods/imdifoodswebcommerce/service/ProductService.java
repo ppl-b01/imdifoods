8992242181eb30c3ce9ffd3e83d3e08d46d3a1e7
fathanm10 <fathan.muhammad01@ui.ac.id>
2023-03-02 21:10:21 +0700

    [GREEN] implement productService

--- src/main/java/com/imdifoods/imdifoodswebcommerce/service/ProductService.java
+++ src/main/java/com/imdifoods/imdifoodswebcommerce/service/ProductService.java
@@ -3,6 +3,8 @@ package com.imdifoods.imdifoodswebcommerce.service;
 import com.imdifoods.imdifoodswebcommerce.model.Product;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 public interface ProductService {
-    Product saveProduct(Product product, MultipartFile imageFile);
+    Product saveProduct(Product product, MultipartFile imageFile) throws IOException;
 }
--- src/main/java/com/imdifoods/imdifoodswebcommerce/service/ProductServiceImpl.java
+++ src/main/java/com/imdifoods/imdifoodswebcommerce/service/ProductServiceImpl.java
@@ -6,13 +6,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @Service
 public class ProductServiceImpl implements ProductService {
+
+    public static String UPLOAD_DIRECTORY = (System.getProperty("user.dir") + File.separator + "uploads");
     @Autowired
     private ProductRepository productRepository;
 
     @Override
-    public Product saveProduct(Product product, MultipartFile imageFile) {
+    public Product saveProduct(Product product, MultipartFile imageFile) throws IOException{
+        if (imageFile == null) {
+            return product;
+        }
+        Files.createDirectories(Paths.get(UPLOAD_DIRECTORY));
+        Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, String.valueOf(product.getId()));
+        Files.write(fileNameAndPath, imageFile.getBytes());
+        productRepository.save(product);
         return product;
     }
 }
