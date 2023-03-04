e198b4f52e84bf6bc8a76368f1ad760c05b218ba
muhammad.dhafin01 <muhammad.dhafin01@ui.ac.id>
2023-03-04 17:32:36 +0700

    [REFACTOR] configure environment variables

--- src/main/java/com/imdifoods/imdifoodswebcommerce/controller/LoginController.java
+++ src/main/java/com/imdifoods/imdifoodswebcommerce/controller/LoginController.java
@@ -1,12 +1,10 @@
 package com.imdifoods.imdifoodswebcommerce.controller;
 
 
-
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
 import java.util.Map;
 
 @RestController
