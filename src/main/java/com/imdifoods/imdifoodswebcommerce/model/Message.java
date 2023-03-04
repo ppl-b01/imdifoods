bd339fa819127192dd6347a7c87ce1b93e84fc0e
David Alexander <david.alexander01@ui.ac.id>
2023-03-04 20:04:05 +0700

    Resolve PBI-30 "Database creation"

---
+++ src/main/java/com/imdifoods/imdifoodswebcommerce/model/Message.java
@@ -0,0 +1,39 @@
+package com.imdifoods.imdifoodswebcommerce.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@Table(name = "message")
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "isi")
+    private String isi;
+
+    @Column(name = "email")
+    private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Message message = (Message) o;
+        return id != null && Objects.equals(id, message.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
