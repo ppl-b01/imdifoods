cac99ce36e39b617c54a94f675a0dd2f4dc0bb48
David Alexander <david.alexander01@ui.ac.id>
2023-03-14 17:51:36 +0700

    [RED] add negative test for send message

--- src/test/java/com/imdifoods/imdifoodswebcommerce/controller/MessageControllerTest.java
+++ src/test/java/com/imdifoods/imdifoodswebcommerce/controller/MessageControllerTest.java
@@ -6,11 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(MessageController.class)
 public class MessageControllerTest {
@@ -30,8 +31,8 @@ public class MessageControllerTest {
                 .param("sender-email", SENDER_EMAIL)
                 .param("sender-message", SENDER_MESSAGE);
 
-        mockMvc.perform(postRequest)
-                .andExpect(status().is3xxRedirection());
+        MvcResult result = mockMvc.perform(postRequest).andReturn();
+        assertEquals(200, result.getResponse().getStatus());
     }
 
     @Test
@@ -40,10 +41,20 @@ public class MessageControllerTest {
                 .post("/message/send")
                 .param("sender-email", SENDER_EMAIL)
                 .param("sender-message", SENDER_MESSAGE);
-        mockMvc.perform(postRequest)
-                .andExpect(status().is3xxRedirection());
+        MvcResult result = mockMvc.perform(postRequest).andReturn();
 
         verify(messageService).saveMessage(SENDER_EMAIL, SENDER_MESSAGE);
+        assertEquals(200, result.getResponse().getStatus());
+    }
+
+    @Test
+    void whenPostMessageNoEmail_ReturnBadRequest() throws Exception {
+        MockHttpServletRequestBuilder postRequest = MockMvcRequestBuilders
+                .post("/message/send")
+                .param("sender-message", SENDER_MESSAGE);
+        MvcResult result = mockMvc.perform(postRequest).andReturn();
+
+        assertEquals(400, result.getResponse().getStatus());
     }
 }
 
