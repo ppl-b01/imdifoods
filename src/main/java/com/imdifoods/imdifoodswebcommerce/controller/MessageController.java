ebd999ebaf889242d67eb403f67ca1610d712dce
David Alexander <david.alexander01@ui.ac.id>
2023-03-14 18:05:35 +0700

    [REFACTOR] use ajax to post form so no page refresh needed

--- src/main/java/com/imdifoods/imdifoodswebcommerce/controller/MessageController.java
+++ src/main/java/com/imdifoods/imdifoodswebcommerce/controller/MessageController.java
@@ -1,7 +1,9 @@
 package com.imdifoods.imdifoodswebcommerce.controller;
 
+import com.imdifoods.imdifoodswebcommerce.model.Message;
 import com.imdifoods.imdifoodswebcommerce.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +16,9 @@ public class MessageController {
     MessageService messageService;
 
     @PostMapping("/send")
-    public String saveMessage(@RequestParam("sender-email") String email,
-                                                  @RequestParam("sender-message") String message) {
-        System.out.println("message saved " + email);
-        messageService.saveMessage(email, message);
-        return "redirect:/";
+    public ResponseEntity<Message> saveMessage(@RequestParam("sender-email") String email,
+                                               @RequestParam("sender-message") String message) {
+        Message messageObject = messageService.saveMessage(email, message);
+        return ResponseEntity.ok(messageObject);
     }
 }
